cfdac88fa4ecf9030607851436631b7b91d67cc5
Brad Chase <brad@these8bits.com>
2024-03-08 14:32:28 -0500

    add `mix.exs` metadata & license

--- mix.exs
+++ mix.exs
@@ -7,13 +7,17 @@ defmodule Phoenix.LiveView.BarcodeScanner.MixProject do
       version: "0.2.0",
       elixir: "~> 1.16",
       start_permanent: false, # Mix.env() == :prod,
+      build_embedded: false, # Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
+      name: "Phoenix LiveView Barcode Scanner",
+      source_url: github_url(),
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger],
     ]
   end
 
@@ -22,5 +26,21 @@ defmodule Phoenix.LiveView.BarcodeScanner.MixProject do
       {:phoenix, "~> 1.7.2"},
       {:phoenix_live_view, "~> 0.18.16"},
     ]
+  end
+
+  defp description() do
+    "Capture USB barcode scanner HID input in Phoenix LiveView"
+  end
+
+  defp package() do
+    [
+      files: ~w(lib test mix.* README* LICENSE* .formatter.exs),
+      licenses: ["MIT"],
+      links: %{"GitHub" => github_url()},
+    ]
+  end
+
+  defp github_url() do
+    "https://github.com/bchase/phoenix_liveview_barcode_scanner/tree/master"
   end
 end
